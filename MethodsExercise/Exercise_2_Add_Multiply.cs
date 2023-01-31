4546681872fba43157932b858fe7c92a70b27d47
Jack Kessler <jack_kessler@hotmail.com>
2023-01-31 13:55:54 -0600

    Exercise 2 addition

---
+++ MethodsExercise/Exercise_2_Add_Multiply.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Exercise_2_Add_Multiply
+    {
+        public static int Add(int num1, int num2)
+        {
+            int numSum = num1 + num2;
+            Console.WriteLine(numSum);
+            return numSum;
+        }
+
+        public static int Multiply(int num3, int num4)
+        {
+            int numProd = num3 * num4;
+            Console.WriteLine(numProd);
+            return numProd;
+        }
+
+        public static int AddAdvanced(params int[] ListNumbers)
+        {
+            int totalSum = 0;
+
+            foreach (int i in ListNumbers)
+            {
+                totalSum += i;
+            }
+            Console.WriteLine(totalSum);
+            return totalSum;
+        }
+
+        public static int MultiplyAdvanced(params int[] ListNumbers)
+        {
+            int totalProduct = 1;
+
+            foreach (int i in ListNumbers)
+            {
+                totalProduct *= i;
+            }
+            Console.WriteLine(totalProduct);
+            return totalProduct;
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Exercise_1_Fun_Story.StoryCreator();
+            Exercise_2_Add_Multiply.Add(5,11);
+            Exercise_2_Add_Multiply.Multiply(5,11);
+            Exercise_2_Add_Multiply.AddAdvanced(1,2,3,4);
+            Exercise_2_Add_Multiply.MultiplyAdvanced(1, 2, 3, 4, 5);
         }
+
     }
 }
